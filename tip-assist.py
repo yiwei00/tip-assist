e80df05e8a784d09fc7165fc5e824f13535ddba6
Yi Wei <yiwei.yiw@gmail.com>
2024-10-25 16:31:36 -0400

    Add hrs, tip rate, and savefile

--- tip-assist.py
+++ tip-assist.py
@@ -1,18 +1,27 @@
 from dataclasses import dataclass
+import csv
+import os.path
 # constants
 SCRIPT_NAME = 'tip-assist'
+SAVEFILE_HEADER = ['last_name', 'first_name', 'hours']
+SAVEFILE_NAME = 'session.csv'
+# commands and keywords
 EXIT_COMMANDS = set(['quit', 'exit'])
-INFO_COMMANDS = set(['info'])
+INFO_COMMANDS = set(['info', 'help'])
+LIST_COMMANDS = set(['list', 'ls'])
 ADD_COMMANDS = set(['add'])
+SET_COMMANDS = set(['set'])
+UPDATE_COMMANDS = set(['update'])
 REMOVE_COMMANDS = set(['remove', 'rem', 'rm'])
 EMPLOYEE_KEYWORDS = set(['employee', 'employees'])
+HOUR_KEYWORDS = set(['hour', 'hr', 'hours', 'hrs'])
+DONE_KEYWORDS = set(['done', ''])
 
 #region data classes
 @dataclass
 class employee_t:
     last_name: str
     first_name: str
-    middle_initial: str = ''
     tippable_hrs: float = 0.0
     tip_rate: float = 0.0
 
@@ -47,15 +56,30 @@ default_stack = {
 #endregion
 
 # global state
-employees = []
+employee_list = []
+total_hours = 0.0
 tips = []
 
-def display_employees():
-    for i, employee in enumerate(employees):
-        print(f'{i + 1:>2}. {employee.last_name[:9] + ",":<10} {employee.first_name:<10}')
+def sort_employees():
+    employee_list.sort(key=lambda e: f'{e.last_name}, {e.first_name}')
 
-# command to add employee
+def calc_tip_rate():
+    global total_hours
+    total_hours = 0.0
+    for e in employee_list:
+        total_hours += e.tippable_hrs
+    for e in employee_list:
+        e.tip_rate = 0.0 if total_hours == 0 else e.tippable_hrs/total_hours
+
+
+#region employee list management
+def display_employees():
+    print(f'{"":2}  {'Last Name':<13} {"First Name":<13} {"Tip Rate":<8}')
+    for i, employee in enumerate(employee_list):
+        print(f'{i + 1:>2}. {employee.last_name[:12] + ",":<13} {employee.first_name[:12]:<13} {employee.tip_rate:<3.3f}')
+
 def add_employees():
+    global employee_list
     print('Existing employees: ')
     display_employees()
     print('Enter new employee name in the format of "last,first"')
@@ -65,19 +89,20 @@ def add_employees():
     while not done:
         raw_name_str = input('> ')
         tokenized_name = raw_name_str.strip().split(',')
-        if tokenized_name[0] == 'done':
+        if tokenized_name[0] in DONE_KEYWORDS:
             done = True
         elif len(tokenized_name) != 2:
             print('unrecognized name format')
         else:
-            employees.append(employee_t(
-                last_name= tokenized_name[0].strip(),
-                first_name=tokenized_name[1].strip()
+            employee_list.append(employee_t(
+                last_name= tokenized_name[0].strip().upper(),
+                first_name=tokenized_name[1].strip().upper()
             ))
     # sort employees via last name
-    employees.sort(key=lambda e: e.last_name)
+    sort_employees()
 
 def remove_employees():
+    global employee_list
     done = False
     while not done:
         print('Employee list: ')
@@ -88,14 +113,14 @@ def remove_employees():
         invalid_reason = ''
         raw_str = input('> ')
         tokenized_input = raw_str.strip().split()
-        if tokenized_input[0] == 'done': #exit
+        if tokenized_input[0] == 'done' or len(tokenized_input) == 0: #exit
             done = True
         # get index to remove
         elif tokenized_input[0].isdigit():
             employee_index = int(tokenized_input[0]) - 1
             # check boundary
-            if employee_index >= 0 and employee_index < len(employees):
-                employees.pop(employee_index)
+            if employee_index >= 0 and employee_index < len(employee_list):
+                employee_list.pop(employee_index)
             else:
                 is_invalid_command = True
                 invalid_reason = 'index out of bound'
@@ -105,10 +130,61 @@ def remove_employees():
         if is_invalid_command:
             print('ERROR: ' + invalid_reason)
             print()
+#endregion
 
+def set_tippable_hrs():
+    global employee_list
+    # set tippable hrs
+    for employee in employee_list:
+        print(f'Current tippable hours for {employee.last_name}, {employee.first_name} is: {employee.tippable_hrs}')
+        while True: # attempt to set hrs
+            print(f'Set new hours for {employee.first_name}')
+            raw_input = input('New hours (hit enter to keep current): ').strip()
+            if raw_input == "":
+                break
+            try:
+                hours = float(raw_input)
+                employee.tippable_hrs = hours
+                break #on success
+            except ValueError:
+                print(f'Unrecognized number {raw_input}')
+    # set tip percent
+    calc_tip_rate()
 
-# main
+#region file IO
+def load_session_from_file(filename):
+    global employee_list
+    employee_list = []
+    with open(filename, 'r') as csvfile:
+        savefile_reader = csv.DictReader(csvfile, SAVEFILE_HEADER)
+        for employee in savefile_reader:
+            print(f'adding {employee['first_name']}')
+            new_employee = employee_t(
+                last_name=employee['last_name'],
+                first_name=employee['first_name'],
+                tippable_hrs=float(employee['hours'])
+            )
+            employee_list.append(new_employee)
+    sort_employees()
+    calc_tip_rate()
+
+def save_session_to_file(filename):
+    with open(filename, 'w') as csvfile:
+        savefile_writer = csv.DictWriter(csvfile, SAVEFILE_HEADER)
+        for employee in employee_list:
+            new_entry = {
+                'last_name': employee.last_name,
+                'first_name': employee.first_name,
+                'hours': employee.tippable_hrs
+            }
+            savefile_writer.writerow(new_entry)
+#endregion file IO
+
+#region main function
 if __name__ == '__main__':
+    # attempt to load session from file
+    if os.path.isfile(SAVEFILE_NAME):
+        load_session_from_file(SAVEFILE_NAME)
     quit = False
     while not quit:
         # read stdin
@@ -120,6 +196,7 @@ if __name__ == '__main__':
         # extract commands/args
         command = tokenized_input[0]
         arguments = tokenized_input[1:]
+        arg_count = len(arguments)
         is_invalid_command = False
         invalid_command_reason = ''
         # process commands
@@ -128,15 +205,29 @@ if __name__ == '__main__':
             continue
         elif command in INFO_COMMANDS:
             display_employees()
+        elif command in LIST_COMMANDS:
+            display_employees()
         elif command in ADD_COMMANDS:
-            if arguments[0] in EMPLOYEE_KEYWORDS:
+            if arg_count < 1:
+                is_invalid_command = True
+                invalid_command_reason = f'command requires at least 1 argument'
+            elif arguments[0] in EMPLOYEE_KEYWORDS:
                 add_employees()
             else:
                 is_invalid_command = True
                 invalid_command_reason = f'unknown argument "{arguments[0]}"'
         elif command in REMOVE_COMMANDS:
-            if arguments[0] in EMPLOYEE_KEYWORDS:
+            if arg_count < 1:
+                is_invalid_command = True
+                invalid_command_reason = f'command requires at least 1 argument'
+            elif arguments[0] in EMPLOYEE_KEYWORDS:
                 remove_employees()
+            else:
+                is_invalid_command = True
+                invalid_command_reason = f'unknown argument "{arguments[0]}"'
+        elif command in SET_COMMANDS:
+            if arguments[0] in HOUR_KEYWORDS:
+                set_tippable_hrs()
             else:
                 is_invalid_command = True
                 invalid_command_reason = f'unknown argument "{arguments[0]}"'
@@ -146,3 +237,6 @@ if __name__ == '__main__':
         # print invalid command message
         if is_invalid_command:
             print('Invalid command: ' + invalid_command_reason)
+    # save session to file
+    save_session_to_file(SAVEFILE_NAME)
+#endregion
